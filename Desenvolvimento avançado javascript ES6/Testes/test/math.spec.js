2d40aa925aec656b227a90f3a9f9f9930743443c
Pedro Daniel Camargos Soares <48827474+PedroDCS@users.noreply.github.com>
2021-10-22 15:49:38 -0300

    chai

--- Desenvolvimento avançado javascript ES6/Testes/test/math.spec.js
+++ Desenvolvimento avançado javascript ES6/Testes/test/math.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const expect = require('chai').expect
 const Math = require('../math.js');
 let value = 0;
 describe('classe matematica', function () {
@@ -12,7 +13,8 @@ describe('classe matematica', function () {
 
         value = 5;
         math.sum(5, value, (value) => {
-            assert.equal(value, 10)
+            expect(value).to.equal(10)
+            //assert.equal(value, 10)
             done();
         });
 
@@ -20,9 +22,22 @@ describe('classe matematica', function () {
 
     //it.skip('Multiplica', function () {
     //it.only('Multiplica', function () {
-    it('Multiplica', function () {
-
+    it.only('Multiplica', function () {
         const math = new Math();
-        assert.equal(math.multiply(value, 5), 0)
+        const obj = {
+            name: "Pedro Daniel"
+        }
+        const obj2 = {
+            name: "Pedro Daniel"
+        }
+
+
+
+        expect(obj).to.have.property('name').equal('Pedro Daniel')
+        expect(obj).to.deep.equal(obj2)
+        expect(math.multiply(value, 5)).to.equal(0)
+        //assert.equal(math.multiply(value, 5), 0)
+
+
     });
 })
